74f14872dbbe3ddcc4a8c37630c36978d68b4132
Thaylla <thayllachristineo@gmail.com>
2019-02-11 14:56:48 -0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-
+import './index.scss'
 export default class App extends Component {
   render() {
-    return (< />)
+    return console.log("tá funcionado")
   }
 }
