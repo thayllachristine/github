e197c67f88e1ed66ba92c5214f57fb698f65ed9a
Thaylla <thayllachristineo@gmail.com>
2019-02-12 22:24:38 -0200

    add/update search components

--- src/components/data/search/label/label.jsx
+++ src/components/data/search/label/label.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-import './input.scss'
+import './label.scss'
+import search from './search.svg'
 
 export default class Input extends Component {
     render() {
         return (
             <label htmlFor='searchInput'>
+                <img src={search} alt='Search icon'/>
                 {this.props.children}
             </label>
         )
