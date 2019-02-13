3f8e50c2698113a77313045852d5bf3d8b72c4bf
Thaylla <thayllachristineo@gmail.com>
2019-02-12 23:32:56 -0200

    add repo component

---
+++ src/components/data/repo/repo.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import repo from '../../../resource/repo.jsx'
+import code from './code.svg'
+import './repo.scss'
+
+
+export default class Repo extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            infos: []
+        }
+    }
+
+
+    componentDidMount() {
+        repo()
+            .then((resp) => {
+                this.setState({
+                    infos: resp.data
+                })
+                console.log(this.state.infos)
+            })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className='repo'>
+                    <RepoListItems infos={this.state.infos} />
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+
+const RepoListItems = (props) => {
+    const items = props.infos.map((info, id) =>
+        <React.Fragment key={id}>
+            <li className='repo-list__item' >
+                <p className='repo__name'>{info.name}</p>
+                <p className='repo__description'>{info.description}</p>
+                <div className='repo__top'>
+                    <img src={code} alt='Code symbol' className='repo__language-img' />
+                    <p className='repo__language'>{info.language}</p>
+                    <p className='repo__forks'>{info.forks}</p>
+                </div>
+            </li>
+            <div className='repo__line' />
+        </React.Fragment>
+    )
+    return (
+        <ul className='repo-list'>
+            {items}
+        </ul>
+    )
+}
