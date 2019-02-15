2b7904bb6205e26ea9fc0502112b4e9551c7c824
Thaylla <thayllachristineo@gmail.com>
2019-02-14 22:22:00 -0200

    changes homepage

---
+++ README.md
@@ -0,0 +1,13 @@
+## Tecnologias utilizadas: 
+- Webpack
+- React
+- SASS
+- Consumo da API do GitHub 
+
+## Config pra visualizar o projeto
+```
+git clone https://github.com/thayllachristine/github.git
+cd superagendador
+npm start / yarn start 
+localhost:3333
+```
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,22 +2,36 @@ import React, { Component } from 'react'
 import './home.scss'
 
 import Navbar from './../components/nav/navbar.jsx'
-import Options from './../components/data/options/options.jsx'
+import Row from './../components/grid/row/row.jsx'
+import Column from './../components/grid/row/column/column.jsx'
 import User from '../components/data/user/user.jsx'
-import Container from './../components/container/container.jsx'
+import Options from './../components/data/options/options.jsx'
 import Input from './../components/data/search/input/input.jsx'
 import Repo from '../components/data/repo/repo.jsx'
 
 
+const kkk = {
+    border: '1px solid red',
+}
+
+
 export default class Home extends Component {
+
+
     render() {
         return (
             <React.Fragment>
                 <Navbar />
-                <User/>
-                <Options />
-                    <Input />
-                    <Repo />
+                <Row>
+                    <Column>
+                        <User />
+                    </Column>
+                    <Column>
+                        <Options />
+                        <Input />
+                        <Repo />
+                    </Column>
+                </Row>
             </React.Fragment >
         )
     }
