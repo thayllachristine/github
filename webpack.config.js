d7fc0e46341aca120b65365fc9c46e52bb3d67eb
Thaylla Christine <thayllachristineo@gmail.com>
2019-11-10 19:18:55 -0300

    refactor: remove grave accent n change space indentation

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-const path = require(`path`)
+const path = require('path')
 
 module.exports = {
-    mode: `development`,
-    entry: `./src/index.js`,
-    output: {
-        path: path.resolve(__dirname, `public`),
-        filename: `bundle.js`
-    },
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
 
-    devServer: {
-        inline: true,
-        contentBase: `./public`,
-        port: 3333,
-    },
+  devServer: {
+    inline: true,
+    contentBase: './public',
+    port: 3000,
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.(scss)$/,
-                use: [
-                    `style-loader`,
-                    `css-loader`,
-                    `sass-loader`
-                ],
-            },
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: `babel-loader`,
-                query: {
-                    presets: [`es2015`, `react`],
-                    plugins: ['transform-class-properties']
-                }
-            },
-            {
-                test: /\.(gif|png|jpe?g|svg)$/,
-                loader: `file-loader`
-            }
-        ]
-    }
+  module: {
+    rules: [
+      {
+        test: /\.(scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-class-properties']
+        }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/,
+        loader: 'file-loader'
+      }
+    ]
+  }
 }
