350a0a65c85c92f8e1f411c80da42f488d65e489
Thaylla Christine <thayllachristineo@gmail.com>
2019-11-11 21:24:17 -0300

    refactor: transform App in stateless component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React, { Component } from 'react'
-import './index.scss'
+import React, { Fragment } from 'react';
+import GlobalStyle from './helpers/GlobalStyle';
 import Home from './pages/home.jsx';
 
-export default class App extends Component {
-  render() { return <Home /> }
-}
+const App = () => (
+  <Fragment>
+    <GlobalStyle />
+    <Home />
+  </Fragment>
+);
+
+export default App;
