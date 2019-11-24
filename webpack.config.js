79942ddb4132bff5785a8ab4e66dcafb72cf9ae3
Thaylla Christine <thayllachristineo@gmail.com>
2019-11-24 16:26:14 -0300

    build: remove babel-plugin-transform-class-properties

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react'],
-          plugins: ['transform-class-properties']
+          presets: ['es2015', 'react']
         }
       },
       {
