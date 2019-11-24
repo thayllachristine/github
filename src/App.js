deb54be96775057debf197314fbfb491bbe8950d
Thaylla Christine <thayllachristineo@gmail.com>
2019-11-24 16:19:23 -0300

    feat: include navbar component n homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import GlobalStyle from './helpers/GlobalStyle';
-import Home from './pages/home.jsx';
+import Navbar from './../src/components/Navbar';
+import Home from './pages/Home';
 
 const App = () => (
   <Fragment>
     <GlobalStyle />
+    <Navbar />
     <Home />
   </Fragment>
 );
